bf2a7d1351716d0fee6b01e6da9941490c69a549
ofir <cassuto26@gmail.com>
2021-12-11 19:22:39 +0200

    wip

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -10,8 +10,8 @@ export default function Dashboard(props) {
       <BrowserRouter>
         <Header></Header>
         <Routes>
-          <Route path="/" element={<Home dispatch={props.dispatch} currentCityId ={props.state.currentCityId} searchOptions={props.state.searchOptions} currentWeather={props.state.currentWeather} favorites={props.state.favoritesList} currentCity={props.state.currentCity}/>} />
-          <Route path="/Favorites" element={<Favorites state={props.state} dispatch={props.dispatch} />} />
+          <Route path="ofir-salamon-06-12-2021/" element={<Home dispatch={props.dispatch} currentCityId ={props.state.currentCityId} searchOptions={props.state.searchOptions} currentWeather={props.state.currentWeather} favorites={props.state.favoritesList} currentCity={props.state.currentCity}/>} />
+          <Route path="ofir-salamon-06-12-2021/Favorites" element={<Favorites state={props.state} dispatch={props.dispatch} />} />
         </Routes>
       </BrowserRouter>
     </>
